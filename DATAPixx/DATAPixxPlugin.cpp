d9b6374b710498a0f9d325bdf18acc1090628279
Christopher Stawarz <cstawarz@mit.edu>
2020-02-25 14:18:24 -0500

    Basic build setup

---
+++ DATAPixx/DATAPixxPlugin.cpp
@@ -0,0 +1,27 @@
+//
+//  DATAPixxPlugin.cpp
+//  DATAPixx
+//
+//  Created by Christopher Stawarz on 2/25/20.
+//  Copyright © 2020 The MWorks Project. All rights reserved.
+//
+
+//#include "DATAPixxDevice.hpp"
+
+
+BEGIN_NAMESPACE_MW
+
+
+class DATAPixxPlugin : public Plugin {
+    void registerComponents(boost::shared_ptr<ComponentRegistry> registry) override {
+        //registry->registerFactory<StandardComponentFactory, DATAPixxDevice>();
+    }
+};
+
+
+extern "C" Plugin* getPlugin() {
+    return new DATAPixxPlugin();
+}
+
+
+END_NAMESPACE_MW
